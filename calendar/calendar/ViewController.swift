8232ccc73584d7560acae16c33b7245f7b147f5d
Julien Levy <julienlevy@ymail.com>
2016-01-23 10:09:10 +0100

    Added xcode project

---
+++ calendar/calendar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  calendar
+//
+//  Created by Julien Levy on 23/01/2016.
+//  Copyright © 2016 Julien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
