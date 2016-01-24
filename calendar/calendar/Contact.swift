7420768c4f5f7647c4fb80de37b0b6f1e7bbe517
Julien Levy <julienlevy@ymail.com>
2016-01-24 15:44:20 +0100

    Event object

---
+++ calendar/calendar/Contact.swift
@@ -0,0 +1,23 @@
+//
+//  Contact.swift
+//  calendar
+//
+//  Created by Julien Levy on 24/01/2016.
+//  Copyright © 2016 Julien. All rights reserved.
+//
+
+import Foundation
+
+class Contact {
+    var firstName: String
+    var lastName: String
+    var email: String
+    
+    var company: String? //Should be a company object
+    
+    init(contactFName: String, contactLName: String, contactEmail: String) {
+        self.firstName = contactFName
+        self.lastName = contactLName
+        self.email = contactEmail
+    }
+}
---
+++ calendar/calendar/SimplifiedEvent.swift
@@ -0,0 +1,28 @@
+//
+//  SimplifiedEvent.swift
+//  calendar
+//
+//  Created by Julien Levy on 24/01/2016.
+//  Copyright © 2016 Julien. All rights reserved.
+//
+
+import Foundation
+
+class Event {
+    var title: String
+    var date: NSDate
+    var duration: NSDate
+    var containingCalendar: String
+    var description: String = ""
+    
+    var locationName: String?
+    var locationCoordinate: (Float, Float)?
+    var people: [Contact]?
+    
+    init(eventTitle: String, eventDate: NSDate, eventDuration: NSDate, eventContainingCalendar: String) {
+        self.title = eventTitle
+        self.date = eventDate
+        self.duration = eventDuration
+        self.containingCalendar = eventContainingCalendar
+    }
+}
