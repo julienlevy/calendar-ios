84c17e743bbab41d1585c57062e93c283b1aa1f5
Julien Levy <julienlevy@ymail.com>
2016-01-28 17:39:56 +0100

    Added soon warning banner

---
+++ calendar/calendar/SoonView.swift
@@ -0,0 +1,21 @@
+//
+//  SoonView.swift
+//  calendar
+//
+//  Created by Julien Levy on 28/01/2016.
+//  Copyright © 2016 Julien. All rights reserved.
+//
+
+import UIKit
+
+class SoonView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
